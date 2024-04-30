6c8667212f0a16248ffcd8a8b3de4d31557bfb4d
nisakib07 <websakib07@gmail.com>
2024-04-30 21:39:47 +0600

    First Commit

---
+++ src/Layout/Layout.jsx
@@ -0,0 +1,9 @@
+const Layout = () => {
+  return (
+    <div>
+      <h1>Hello</h1>
+    </div>
+  );
+};
+
+export default Layout;
---
+++ src/Layout/components/Wrapper.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Wrapper = ({ children }) => {
+  return <div className="max-w-[1440px]">{children}</div>;
+};
+
+export default Wrapper;
